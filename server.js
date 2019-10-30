fcc575b96e5ae379ea363ba35bcdd289b8f6d78e
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-31 00:33:45 +0800

    Preparing deployment

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-  }
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//   }
+// });
 
 const dataRoute = require("./api/routes/weatherdata");
 const dataRouteCainta = require("./api/routes/cainta");
